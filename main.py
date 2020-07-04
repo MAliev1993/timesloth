4c3d1031d1843c1ca43ba55d6ac65487902d2cfc
MAliev1993 <m-z-a@yandex.ru>
2020-07-04 18:51:07 +0200

    Optimization: - overhauled the script to use standard bs4 methods - changed logic for sending the email

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import bs4
-import requests
 import smtplib
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 
 
-def getAvailableTimeSlot(rows: list) -> str:
-    for week_row in rows:
-        days = week_row.contents
-        for day in days:
-            types = list()
-            types.append(type(day))
-            if type(day) is bs4.element.Tag and len(day.contents) > 0:
-                text = str(day.contents[0].contents[0])
-                if text != 'Keine freien Termine am ':
-                    return str(day.contents[1] + day.contents[2].contents[0])
-    return "No available time slots"
+def check(calendar_rows: list, link: str):
+    for day in calendar_rows:
+        if day.text != '' and day.text[:24] != 'Keine freien Termine am ':
+            sendAlert('Subject: Time slot available! \n\n{} Text: {}'.format(link, day.text))
+            break
 
 
 def sendAlert(message: str):
@@ -31,29 +23,22 @@ options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1200x600')
 
-link = 'https://www.muenchen.de/rathaus/terminvereinbarung_fs.html?&loc=FS&ct=1071898'
+url = 'https://www.muenchen.de/rathaus/terminvereinbarung_fs.html?&loc=FS&ct=1071898'
+waiting_time = 3
 
 driver = webdriver.Chrome(options=options)
-driver.get(link)
+driver.get(url)
 
-driver.implicitly_wait(5)
+driver.implicitly_wait(waiting_time)
 
 driver.switch_to.frame(driver.find_element_by_xpath('//*[@id="appointment"]'))
 
 driver.find_element_by_xpath('//*[@id="F00e214c9f52bf4cddab8ebc9bbb11b2b"]/fieldset/input[2]').click()
 
-driver.implicitly_wait(3)
+driver.implicitly_wait(waiting_time)
 
 soup = bs4.BeautifulSoup(driver.page_source, 'html.parser')
 
-week_rows = soup.contents[0].contents[2].contents[13].contents[1].contents[3].contents[1].contents[0].contents[1].contents[3].contents
-
-result = getAvailableTimeSlot(week_rows)
-#result = 'No available time slots'
-# git test
-
-if result != 'No available time slots':
-    message = 'Subject: Available time slot: {}\n\n<a href="{}" target="_blank">link</a>'.format(result, link)
-    sendAlert(message)
+check(soup.findAll('td', {'class': 'nat_calendar'}), url)
 
 driver.quit()
